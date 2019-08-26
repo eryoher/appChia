dfec7e82e01cc1d19792d20caa1ce7b2ee2dc4fe
Sergio Portela <sergio.portela@myprocess.net>
2019-08-26 12:21:01 -0500

    Cambios sin guardar

--- src/components/category.js
+++ src/components/category.js
@@ -5,29 +5,30 @@ import { Link } from 'react-router';
 
 class Category extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = { 'nextPage' : 0 }
+        this.state = { 'nextPage': 0 }
     }
 
-    componentWillMount(){
-        this.props.getCategory( this.props.params.id );    }
+    componentWillMount() {
+        this.props.getCategory(this.props.params.id);
+    }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
         const { category } = this.props
 
-        if(  this.state.nextPage == 0 ){
-            if( category !== undefined ){
+        if (this.state.nextPage == 0) {
+            if (category !== undefined) {
                 var categories = category.product.categories;
                 //primera pagina
                 var inicio = categories[0].id
                 var next = 0;
 
                 categories.forEach((catg, index) => {
-                    if( inicio > catg.id ){
+                    if (inicio > catg.id) {
                         inicio = catg.id
                     }
-                    if( catg.id == this.props.params.id ){
+                    if (catg.id == this.props.params.id) {
                         next = (categories[index + 1] !== undefined) ? categories[index + 1].id : inicio
                     }
 
@@ -35,42 +36,42 @@ class Category extends Component {
 
                 //console.log('actual:::', this.props.params.id);
                 //console.log('next >>>>', next);
-                this.setState({'nextPage' : next});
+                this.setState({ 'nextPage': next });
             }
         }
 
     }
-    renderItems(){
+    renderItems() {
         const { category } = this.props;
         var rows = [];
-        const divList = (category.product.id == 1 || category.product.id == 5 ) ? true : false
-        const classDiv = ( divList ) ? 'col-12' : 'col-12 col-sm-12 col-md-6 col-lg-6 col-lx-6 mt-5'
-        const classItem = ( divList ) ? 'category-item' : 'item'
-        category.items.forEach((item , index)=> {
+        const divList = (category.product.id == 1 || category.product.id == 5) ? true : false
+        const classDiv = (divList) ? 'col-12' : 'col-12 col-sm-12 col-md-6 col-lg-6 col-lx-6 mt-5'
+        const classItem = (divList) ? 'category-item' : 'item'
+        category.items.forEach((item, index) => {
             rows.push(
                 <div className={`${classDiv}`} key={item.id}>
                     <Link to={`/item/${item.id}`}>
                         <div className={`${classItem}`}>
-                            <p className="overflow-wrap">
-                                { (!divList) ? item.name.split(' ').map(function(item, key) {
+                            <p className="overflow-wrap text-center">
+                                {(!divList) ? item.name.split(' ').map(function (item, key) {
                                     return (
                                         <span key={key}>
-                                        {item}
-                                        <br/>
+                                            {item}
+                                            <br />
                                         </span>
                                     )
-                                }):item.name }
+                                }) : item.name}
                             </p>
                         </div>
                     </Link>
                 </div>
             );
         });
-        
+
         return rows
     }
 
-    handleRedirect( path ){
+    handleRedirect(path) {
         window.location.href = path;
     }
 
@@ -82,23 +83,23 @@ class Category extends Component {
             <div className="category-container">
                 <div className="container">
                     <div className="title-product pt-5">
-                        { (category !== undefined) ? category.product.name : null }
+                        {(category !== undefined) ? category.product.name : null}
                     </div>
 
                     <div className="title-category">
-                        { (category !== undefined) ? category.name : null }
+                        {(category !== undefined) ? category.name : null}
                     </div>
 
                     <div className="items-list row justify-content-start">
-                    { ( this.props.category !== undefined ) ? this.renderItems() : null }
+                        {(this.props.category !== undefined) ? this.renderItems() : null}
                     </div>
                 </div>
                 <div className="container mt-5">
-                    <div className="footer-btns row">
-                        <div className="start col-4" onClick={ () => this.handleRedirect(`/product/${productId}`)} />
-                        <div className="homePage col-4" onClick={ () => this.handleRedirect(`/`)} />
-                        <div className="next col-4" onClick={ () => this.handleRedirect(`/category/${this.state.nextPage}`)} />
-
+                    <div className="row banner"></div>
+                    <div className="footer-btns container row">
+                        <div className="start col-4 col-sm-4 col-md-4 col-lg-4 col-lx-4" onClick={() => this.handleRedirect(`/product/${productId}`)} />
+                        <div className="homePage col-4 col-sm-4 col-md-4 col-lg-4 col-lx-4" onClick={() => this.handleRedirect(`/`)} />
+                        <div className="next col-4 col-sm-4 col-md-4 col-lg-4 col-lx-4" onClick={() => this.handleRedirect(`/category/${this.state.nextPage}`)} />
                     </div>
                 </div>
             </div>
@@ -106,11 +107,11 @@ class Category extends Component {
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
 
     return {
-        category : state.data.category,
+        category: state.data.category,
     }
 }
 
-export default connect (mapStateToProps, actions)(Category);
+export default connect(mapStateToProps, actions)(Category);
